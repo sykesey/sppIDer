f386c9e641254960ad70807a51507c059a073ebf
Ben Sykes <benpsykes@gmail.com>
2019-09-14 14:53:59 +1000

    Fix up Saccharomyces references

--- generate-ref.sh
+++ generate-ref.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pushd ref
-docker run --rm -it --mount type=bind,src=$(pwd),target=/tmp/sppIDer/working --user "$UID:$(id -g $USERNAME)" sppider  combineRefGenomes.py --out SaccharomycesCombo.fasta --key SaccharomycesComboRef.txt
-docker run --rm -it --mount type=bind,src=$(pwd),target=/tmp/sppIDer/working --user "$UID:$(id -g $USERNAME)" sppider  combineRefGenomes.py --out SaccCerUvaEub.fasta --key SaccCerUvaEubRef.txt
+docker run --rm -it --mount type=bind,src=$(pwd)/ref,target=/tmp/sppIDer/working --user "$UID:$(id -g $USERNAME)" sppider  combineRefGenomes.py --out SaccharomycesCombo.fasta --key SaccharomycesComboRef.txt
+docker run --rm -it --mount type=bind,src=$(pwd)/ref,target=/tmp/sppIDer/working --user "$UID:$(id -g $USERNAME)" sppider  combineRefGenomes.py --out SaccCerUvaEub.fasta --key SaccCerUvaEubRef.txt
 popd
