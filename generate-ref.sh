b7139e568523c2a40772d072268721ac5b6acb97
Ben Sykes <benpsykes@gmail.com>
2019-09-14 14:55:24 +1000

    Fix up reference generation process

--- generate-ref.sh
+++ generate-ref.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-pushd ref
-docker run --rm -it --mount type=bind,src=$(pwd)/ref,target=/tmp/sppIDer/working --user "$UID:$(id -g $USERNAME)" sppider  combineRefGenomes.py --out SaccharomycesCombo.fasta --key SaccharomycesComboRef.txt
-docker run --rm -it --mount type=bind,src=$(pwd)/ref,target=/tmp/sppIDer/working --user "$UID:$(id -g $USERNAME)" sppider  combineRefGenomes.py --out SaccCerUvaEub.fasta --key SaccCerUvaEubRef.txt
-popd
+docker run --rm -it --mount type=bind,src=$(pwd)/ref,target=/tmp/sppIDer/working --user "$UID:$(id -g $USERNAME)" benpsykes/sppider  combineRefGenomes.py --out SaccharomycesCombo.fasta --key SaccharomycesComboRef.txt
+docker run --rm -it --mount type=bind,src=$(pwd)/ref,target=/tmp/sppIDer/working --user "$UID:$(id -g $USERNAME)" benpsykes/sppider  combineRefGenomes.py --out SaccCerUvaEub.fasta --key SaccCerUvaEubRef.txt
